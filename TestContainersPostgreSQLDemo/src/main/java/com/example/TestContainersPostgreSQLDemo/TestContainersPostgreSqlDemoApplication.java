9c15fad837195d60451b21285a42cb9f2215786c
Guy <guy@pop-os.localdomain>
2020-10-08 21:46:09 +0200

    Initial commit

---
+++ TestContainersPostgreSQLDemo/src/main/java/com/example/TestContainersPostgreSQLDemo/TestContainersPostgreSqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestContainersPostgreSQLDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestContainersPostgreSqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestContainersPostgreSqlDemoApplication.class, args);
+	}
+
+}
