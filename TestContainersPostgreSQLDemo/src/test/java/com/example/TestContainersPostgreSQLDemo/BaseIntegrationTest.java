183c7e200135af656b741ebc703d6279c87461d4
Guy <guy@pop-os.localdomain>
2020-10-16 22:32:42 +0200

    Create BaseIntegrationTest

---
+++ TestContainersPostgreSQLDemo/src/test/java/com/example/TestContainersPostgreSQLDemo/BaseIntegrationTest.java
@@ -0,0 +1,34 @@
+package com.example.TestContainersPostgreSQLDemo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@ContextConfiguration(initializers = {BaseIntegrationTest.Initializer.class})
+public class BaseIntegrationTest {
+	
+	@Container
+	public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:12.3")
+	      .withInitScript("db/docker/init-db.sql");
+	 
+	
+	static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+		public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+          TestPropertyValues.of(
+            "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl() //,
+//            "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+//            "spring.datasource.password=" + postgreSQLContainer.getPassword()
+          ).applyTo(configurableApplicationContext.getEnvironment());
+      }
+		
+  }
+
+}
